be87c1be237800d4de80b3eb136bd6ef8469f48b
Majesty <87654018+majesty-git@users.noreply.github.com>
2024-06-26 17:10:30 +0200

    Update autostart.sh

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -2,7 +2,5 @@
 
 xrandr --output HDMI-0 --mode 1920x1080 --right-of DP-5
 xrandr --output DP-5 --mode 1920x1080 --rate 74.97
-nitrogen --restore
+feh --bg-fill ~/wallpapers/pic.jpg
 ~/.config/polybar/launch.sh &
-thunar --daemon
-polkit-dumb-agent &
